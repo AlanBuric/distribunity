e51dac422c690d46f05fa0cea981e229548f8f6b
Alan Burić <134159250+AlanBuric@users.noreply.github.com>
2024-03-21 01:30:17 +0100

    Add some content, improve UI - fix and tidy up some CSS code - fix the front page - add content to the front page for visitors and potential users - expand the signup section at work.html - ...introduce new bugs :(

--- frontend/script/index.js
+++ frontend/script/index.js
@@ -1,11 +1,14 @@
-async function updatePageWidth(elementIDs) {
+console.log("Snooping around, are we?");
+
+function updatePageWidth() {
     const ratio = 100 * document.documentElement.clientHeight / document.documentElement.clientWidth;
     const width = Math.min(100, Math.max(50, ratio));
 
-    elementIDs.forEach(element => {
-        document.getElementById(element).style.width = width + '%';
+    document.querySelectorAll(".dyn-max-width").forEach(element => {
+        element.style.width = width + '%';
+        element.style.maxWidth = width + '%';
     });
 }
 
-window.addEventListener('DOMContentLoaded', event => updatePageWidth(['page']));
-window.addEventListener('resize', event => updatePageWidth(['page']));
+window.addEventListener('DOMContentLoaded', event => updatePageWidth());
+window.addEventListener('resize', event => updatePageWidth());
