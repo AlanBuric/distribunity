092ed7db1c15141928f1d49986e7cd54afae0a6d
Alan Burić <134159250+AlanBuric@users.noreply.github.com>
2024-04-17 16:59:41 +0200

    Optimise SVG logos

--- src/components/icons/Logo.vue
+++ src/components/icons/Logo.vue
@@ -1,43 +1,35 @@
 <!-- Note: both width and height properties need to be overriden when necessary. -->
 <!-- Functional header: alt="Distribunity logo, white letter S inside an aquamarine hexagon" viewBox="0 -8 125 150" -->
 <template>
-    <svg width="81px" alt="Distribunity logo, white letter S inside an aquamarine hexagon" version="1.1"
-        viewBox="0 0 81.171873 81.171877" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"
-        xmlns:svg="http://www.w3.org/2000/svg">
-        <defs id="defs1">
-            <linearGradient id="linearGradient1">
-                <stop style="stop-color:#00d4aa;stop-opacity:1;" offset="0" id="stop1" />
-                <stop style="stop-color:#00d3aa;stop-opacity:1;" offset="0.38683674" id="stop3" />
-                <stop style="stop-color:#0094a8;stop-opacity:1;" offset="1" id="stop2" />
+    <svg width="100%" height="100%" viewBox="0 0 81.172 81.172" xmlns:xlink="http://www.w3.org/1999/xlink"
+        xmlns="http://www.w3.org/2000/svg">
+        <defs>
+            <radialGradient xlink:href="#a" id="b" cx="-105.352" cy="-53.014" fx="-105.352" fy="-53.014" r="132.164"
+                gradientTransform="matrix(1 0 0 1.15443 0 8.187)" gradientUnits="userSpaceOnUse" />
+            <linearGradient id="a">
+                <stop style="stop-color:#00d4aa;stop-opacity:1" offset="0" />
+                <stop style="stop-color:#00d3aa;stop-opacity:1" offset=".387" />
+                <stop style="stop-color:#0094a8;stop-opacity:1" offset="1" />
             </linearGradient>
-            <radialGradient xlink:href="#linearGradient1" id="radialGradient2" cx="-105.3523" cy="-53.014046"
-                fx="-105.3523" fy="-53.014046" r="132.16447" gradientTransform="matrix(1,0,0,1.1544321,0,8.1870731)"
-                gradientUnits="userSpaceOnUse" />
-            <filter style="color-interpolation-filters:sRGB" id="filter51" x="-0.091270083" y="-0.075251984"
-                width="1.1825402" height="1.150504">
-                <feFlood result="flood" in="SourceGraphic" flood-opacity="1.000000" flood-color="rgb(255,255,255)"
-                    id="feFlood50" />
-                <feGaussianBlur result="blur" in="SourceGraphic" stdDeviation="2.000000" id="feGaussianBlur50" />
-                <feOffset result="offset" in="blur" dx="0.000000" dy="0.000000" id="feOffset50" />
-                <feComposite result="comp1" operator="in" in="flood" in2="offset" id="feComposite50" />
-                <feComposite result="comp2" operator="over" in="SourceGraphic" in2="comp1" id="feComposite51" />
+            <filter style="color-interpolation-filters:sRGB" id="c" x="-.091" y="-.075" width="1.183" height="1.151">
+                <feFlood result="flood" flood-opacity="1" flood-color="#FFF" />
+                <feGaussianBlur result="blur" in="SourceGraphic" stdDeviation="2" />
+                <feOffset result="offset" in="blur" />
+                <feComposite result="comp1" operator="in" in="flood" in2="offset" />
+                <feComposite result="comp2" in="SourceGraphic" in2="comp1" />
             </filter>
         </defs>
-        <g id="layer1" transform="translate(-73.035165,-94.395863)">
+        <path
+            style="fill:url(#b);stroke:#00f1cd;stroke-width:21.6497;stroke-linecap:butt;stroke-linejoin:round;stroke-dasharray:none;paint-order:stroke markers fill"
+            d="M26.652-129.216-29.139 79-237.357 23.21l55.792-208.217z"
+            transform="rotate(-14.999 225.287 -279.834) scale(.34219)" />
+        <g transform="translate(-54.138 -71.896) scale(.83494)" style="filter:url(#c)">
             <path
-                style="fill:url(#radialGradient2);stroke:#00f1cd;stroke-width:21.6497;stroke-linecap:butt;stroke-linejoin:round;stroke-dasharray:none;paint-order:stroke markers fill"
-                id="path1" d="M 26.652177,-129.21615 -29.13948,79.001149 -237.35678,23.209492 -181.56513,-185.00781 Z"
-                transform="matrix(0.33053123,-0.08856558,0.08856558,0.33053123,153.1376,143.17047)" />
-            <g id="g93" transform="matrix(0.83494039,0,0,0.83494039,18.897029,22.499784)" style="filter:url(#filter51)">
-                <path
-                    style="fill:#ffffff;fill-opacity:1;stroke:none;stroke-width:6.82001;stroke-linecap:butt;stroke-linejoin:round;stroke-dasharray:none;paint-order:stroke markers fill"
-                    id="path92"
-                    d="m 97.537071,105.60047 c 1.1551,4.64622 1.72263,9.41423 2.25476,14.16396 0.427999,4.72132 0.635109,9.488 0.05,14.20692 -0.35463,2.86019 -0.5932,3.48149 -1.27509,6.24307 -0.97425,3.28604 -2.37767,6.41192 -3.88356,9.48131 0,0 4.98071,2.82387 4.98071,2.82387 v 0 c 1.457469,-3.13935 2.850269,-6.31662 3.824849,-9.6459 0.68165,-2.7825 0.93235,-3.45668 1.28492,-6.33658 0.58134,-4.74852 0.37507,-9.54789 -0.002,-14.30202 -0.48135,-4.70055 -1.02124,-9.39957 -1.87453,-14.04962 z" />
-                <path
-                    style="fill:#ffffff;fill-opacity:1;stroke:none;stroke-width:6.82001;stroke-linecap:butt;stroke-linejoin:round;stroke-dasharray:none;paint-order:stroke markers fill"
-                    id="path93"
-                    d="m 92.225271,111.93914 c 3.24147,-2.51421 7.34706,-3.47902 11.226049,-4.53748 4.55675,-1.17746 9.26393,-1.77353 13.94246,-0.98022 2.97204,0.50395 4.37078,0.88898 6.28278,1.96581 1.05904,0.59645 2.73509,2.41148 2.73509,2.41148 1.80217,1.73017 3.06605,3.50088 4.41111,5.57368 1.80019,2.9934 2.65279,6.34577 3.24245,9.75061 0.46039,3.23785 0.59923,6.51322 0.38543,9.77473 -0.3166,3.53043 -1.6321,6.84804 -3.41318,9.88897 -0.4165,0.71111 -0.88823,1.3884 -1.33234,2.08259 -1.3878,1.90985 -2.87236,3.73748 -4.69728,5.25002 -1.86264,1.54382 -3.93329,2.71484 -6.04951,3.86255 -3.84406,1.93964 -7.86338,3.5535 -12.01874,4.69419 -0.85019,0.23339 -1.71153,0.42411 -2.56729,0.63617 -2.65682,0.67554 -5.363239,0.92599 -8.093869,0.89916 l 4.687079,3.40007 v 0 c 2.72507,-0.0673 5.42196,-0.38809 8.06534,-1.08788 5.06934,-1.30875 10.04936,-2.97315 14.68976,-5.43413 1.80877,-1.00267 2.52905,-1.34157 4.23433,-2.53149 2.64807,-1.84779 4.8269,-4.17002 6.65474,-6.82689 0.45239,-0.70593 0.93035,-1.39612 1.35718,-2.1178 1.82989,-3.09398 3.24556,-6.45426 3.61361,-10.05521 0.045,-0.57596 0.10887,-1.15076 0.13496,-1.72789 0.12313,-2.72397 -0.15017,-5.46191 -0.44774,-8.16481 -0.57006,-3.4801 -1.41077,-6.90961 -3.20367,-9.9887 -1.35959,-2.14348 -2.62079,-3.94811 -4.44148,-5.75734 -3.50713,-3.48505 -7.98321,-5.72618 -12.33817,-7.97291 -2.8072,-1.02582 -3.3568,-1.38502 -6.35498,-1.83321 -4.7282,-0.7068 -9.46148,-0.0281 -14.054749,1.14367 -4.04281,1.07253 -8.30852,2.0136 -11.72012,4.5749 z" />
-            </g>
+                style="fill:#fff;fill-opacity:1;stroke:none;stroke-width:6.82001;stroke-linecap:butt;stroke-linejoin:round;stroke-dasharray:none;paint-order:stroke markers fill"
+                d="M97.537 105.6c1.155 4.647 1.723 9.415 2.255 14.164.428 4.722.635 9.488.05 14.207-.355 2.86-.593 3.482-1.275 6.243-.975 3.286-2.378 6.412-3.884 9.482l4.98 2.824c1.458-3.14 2.851-6.317 3.826-9.646.681-2.783.932-3.457 1.285-6.337.581-4.748.375-9.548-.002-14.302-.482-4.7-1.022-9.4-1.875-14.05z" />
+            <path
+                style="fill:#fff;fill-opacity:1;stroke:none;stroke-width:6.82001;stroke-linecap:butt;stroke-linejoin:round;stroke-dasharray:none;paint-order:stroke markers fill"
+                d="M92.225 111.94c3.242-2.515 7.347-3.48 11.226-4.538 4.557-1.178 9.264-1.774 13.943-.98 2.972.503 4.37.888 6.283 1.965 1.059.597 2.735 2.412 2.735 2.412 1.802 1.73 3.066 3.5 4.41 5.573 1.801 2.994 2.654 6.346 3.243 9.751.46 3.238.6 6.513.386 9.775-.317 3.53-1.632 6.848-3.414 9.889-.416.71-.888 1.388-1.332 2.082-1.388 1.91-2.872 3.738-4.697 5.25-1.863 1.544-3.933 2.715-6.05 3.863-3.844 1.94-7.863 3.553-12.018 4.694-.85.233-1.712.424-2.568.636-2.657.676-5.363.926-8.094.9l4.688 3.4c2.725-.068 5.421-.389 8.065-1.088 5.07-1.31 10.05-2.974 14.69-5.435 1.808-1.002 2.529-1.341 4.234-2.531 2.648-1.848 4.827-4.17 6.655-6.827.452-.706.93-1.396 1.357-2.118 1.83-3.094 3.245-6.454 3.613-10.055.045-.576.11-1.15.135-1.728.124-2.724-.15-5.462-.447-8.165-.57-3.48-1.411-6.91-3.204-9.988-1.36-2.144-2.62-3.948-4.441-5.758-3.508-3.485-7.984-5.726-12.339-7.973-2.807-1.025-3.356-1.385-6.355-1.833-4.728-.707-9.461-.028-14.054 1.144-4.043 1.072-8.309 2.013-11.72 4.575z" />
         </g>
     </svg>
 </template>
