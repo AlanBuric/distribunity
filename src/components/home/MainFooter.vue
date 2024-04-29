0bf342341ea3c09637431232ec57994d2680a970
Alan Burić <134159250+AlanBuric@users.noreply.github.com>
2024-04-29 18:43:12 +0200

    Improve MainFooter.vue

--- src/components/home/MainFooter.vue
+++ src/components/home/MainFooter.vue
@@ -1,26 +1,35 @@
+<script setup>
+    import Logo from '../icons/Logo.vue';
+</script>
 <template>
     <div id="footer-wrapper">
         <hr>
         <footer class="page-inner">
-            <section>
-                <h3>Legal</h3>
-                <!---TODO: :link, :visited, :any-link-->
-                <ul>
-                    <li><a href="">Terms of Service</a></li>
-                    <li><a href="">Privacy Policy</a></li>
-                    <li><a href="">Cookie Policy</a></li>
-                    <li><a href="">Impressum</a></li>
-                </ul>
-            </section>
-            <section>
-                <h3>External websites</h3>
-                <ul>
-                    <li><a href="https://github.com/AlanBuric/distribunity">GitHub</a></li>
-                </ul>
-            </section>
-            <section>
-                <h3>Contacts</h3>
-                <p>E-mail the developer: <a href="mailto:aburic1@unipu.hr">aburic1@unipu.hr</a></p>
+            <div id="footer-links">
+                <section>
+                    <h3>Legal</h3>
+                    <!---TODO: :link, :visited, :any-link-->
+                    <ul>
+                        <li><a href="">Terms of Service</a></li>
+                        <li><a href="">Privacy Policy</a></li>
+                        <li><a href="">Cookie Policy</a></li>
+                        <li><a href="">Impressum</a></li>
+                    </ul>
+                </section>
+                <section>
+                    <h3>External websites</h3>
+                    <ul>
+                        <li><a href="https://github.com/AlanBuric/distribunity">GitHub</a></li>
+                    </ul>
+                </section>
+                <section>
+                    <h3>Contacts</h3>
+                    <p>Site issues? <br>E-mail the developer: <a href="mailto:aburic1@unipu.hr">aburic1@unipu.hr</a></p>
+                </section>
+            </div>
+            <section id="footer-end">
+                <Logo></Logo>
+                <p>Copyright Distribunity 2024.</p>
             </section>
         </footer>
     </div>
@@ -40,9 +49,21 @@
 
     footer {
         display: flex;
+        flex-direction: column;
+        gap: 20px;
+        padding: 30px 10px;
+    }
+
+    #footer-links {
+        display: flex;
         justify-content: space-evenly;
-        padding: 30px 10px 100px 10px;
         gap: 2vw;
+    }
+
+    #footer-end {
+        display: flex;
+        flex-direction: column;
+        place-items: center;
     }
 
     ul {
