698b3806c603763682abd75703b4a4c4ce9e84ab
Alan Burić <134159250+AlanBuric@users.noreply.github.com>
2024-08-31 00:35:19 +0200

    feat(build): use LightningCSS CSS transformer

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
-import { visualizer } from "rollup-plugin-visualizer";
+import { fileURLToPath, URL } from 'node:url';
+import { visualizer } from 'rollup-plugin-visualizer';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    visualizer({
-      emitFile: true,
-      filename: 'stats.html'
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+	plugins: [
+		vue(),
+		visualizer({
+			emitFile: true,
+			filename: 'stats.html',
+		}),
+	],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+	css: {
+		transformer: 'lightningcss',
+	},
 });
