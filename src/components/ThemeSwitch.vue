a92344b4e105be414e5a4024c98ecfac3d69b9b5
Alan Burić <134159250+AlanBuric@users.noreply.github.com>
2024-09-15 20:11:40 +0200

    feat: add Pinia auth and global store for ThemeSwitch.vue and future uses
    TODO: save client-side theme preference to the user's user profile in Firestore. This needs to be done safely, because it can be spammed easily.

--- src/components/ThemeSwitch.vue
+++ src/components/ThemeSwitch.vue
@@ -1,28 +1,15 @@
 <script setup lang="ts">
-  import { ref } from 'vue';
   import MoonIcon from './icons/MoonIcon.vue';
   import SunIcon from './icons/SunIcon.vue';
+  import useGlobalStore from '@/store/global';
 
-  const isDark = ref(false);
-
-  const toggleDarkMode = () => {
-    isDark.value = !isDark.value;
-    document.documentElement.classList.toggle('dark', isDark.value);
-    localStorage.setItem('theme', isDark.value ? 'dark' : 'light');
-  };
-
-  // Load the theme on initial load:
-  const savedTheme = localStorage.getItem('theme') === 'dark';
-
-  document.documentElement.classList.toggle('dark', savedTheme);
-
-  isDark.value = savedTheme;
+  const store = useGlobalStore();
 </script>
 
 <template>
-  <button @click="toggleDarkMode">
+  <button @click="store.toggleTheme">
     <MoonIcon
-      v-if="isDark"
+      v-if="store.isDarkTheme()"
       width="1.4em"
       height="1.4em"
       class="fill-gray-300"
---
+++ src/store/auth.ts
@@ -0,0 +1,23 @@
+import { auth, database } from '@/firebase/init';
+import type { User } from '@/types/types';
+import { doc, DocumentReference } from 'firebase/firestore';
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import { useDocument } from 'vuefire';
+
+const useAuthStore = defineStore('auth', () => {
+    const userProfileRef = ref<DocumentReference>();
+    const userProfile = useDocument<User>(userProfileRef, { maxRefDepth: 0 });
+
+    auth.onAuthStateChanged((user) => {
+        if (user) {
+            userProfileRef.value = doc(database, 'users', user.uid);
+        } else {
+            userProfileRef.value = undefined;
+        }
+    });
+
+    return { userProfile, userProfileRef };
+});
+
+export default useAuthStore;
