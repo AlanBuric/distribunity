e7bc4182233b9255e5e3aa3386975a0e70a84b67
Alan Burić <134159250+AlanBuric@users.noreply.github.com>
2024-09-17 03:25:19 +0200

    fix(organization): member should be a collection, not a document

--- src/components/JoinOrganizationForm.vue
+++ src/components/JoinOrganizationForm.vue
@@ -27,7 +27,8 @@
       const targetOrganization = snapshot.docs[0];
       const batch = writeBatch(database);
 
-      batch.update(targetOrganization.ref, { invites: arrayRemove(invitationCode.value), members: arrayUnion(doc(database, 'users', auth.currentUser!.uid)) });
+      batch.update(targetOrganization.ref, { invites: arrayRemove(invitationCode.value) });
+      batch.set(doc(targetOrganization.ref, 'members', auth.currentUser!.uid), { roles: [] });
       batch.update(doc(database, 'users', auth.currentUser!.uid), { organizations: arrayUnion(targetOrganization.ref) });
 
       await batch.commit();
